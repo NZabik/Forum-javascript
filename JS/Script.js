63507c8c4e49a6475476dd3e822f46438ad74707
Nicolas Zabik <melchiah.nico@gmail.com>
2023-10-30 15:55:24 +0100

    changement des boutons de la page d'accueil pour enlever les <a> comme demandé dans W3C

--- JS/Script.js
+++ JS/Script.js
@@ -1,6 +1,9 @@
 let deco = document.getElementById("deco");
 let deco2 = document.getElementById("deco2");
 let deco3 = document.getElementById("deco3");
+let btn = document.getElementById("btn");
+let btn2 = document.getElementById("btn2");
+
 deco.addEventListener("click", function (e) {
     if (localStorage.getItem("Date") == null) {
         alert("Vous êtes déjà déconnecté");
@@ -25,4 +28,18 @@ deco3.addEventListener("click", function (e) {
         alert("Vous êtes déjà connecté");
         e.preventDefault();
     };
+});
+btn2.addEventListener("click", function (e) {
+    document.location.href = "./HTML/register.html";
+        e.preventDefault();
+
+});
+btn.addEventListener("click", function (e) {
+    if (localStorage.getItem("Date") != null) {
+        alert("Vous êtes déjà connecté");
+        e.preventDefault();
+    } else{
+    document.location.href = "./HTML/login.html";
+        e.preventDefault();
+    }
 });
